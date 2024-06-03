aaedbb4ac4442310e5201a3cb60a02d4a9cb34c4
Weedguet Mildort <108565362+weedguetmildort@users.noreply.github.com>
2024-06-02 21:05:39 -0400

    Initial setup

---
+++ src/app.c
@@ -0,0 +1,14 @@
+// Necessary libraries
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+// Main
+int main(void)
+{
+    printf("Hello, World!\n");
+    printf("I am ready! Promotion!\n");
+    return 0;
+}
